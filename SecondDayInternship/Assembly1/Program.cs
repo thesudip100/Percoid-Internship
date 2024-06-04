78f99bdc546913c7e712d57d03097dcafdedbbb0
Sudip Paudel <sudippaudel944@gmail.com>
2024-06-04 14:48:46 +0545

    Second day Practise

---
+++ SecondDayInternship/Assembly1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using FirstDay;
+
+public class Assembly1DerivedClass:baseClass
+    {
+           public void TestAcesssinDerivedClass()
+            {
+                /* Console.WriteLine(address);
+                 Console.WriteLine(job);*/
+                   Console.WriteLine(name);
+                   Console.WriteLine(street);
+                   Console.WriteLine(description);
+                }
+}
+
+public class Assembly1OtherClass
+    {
+        public void TestAccessinOtherClass()
+        {
+           baseClass baseObj = new baseClass();
+        //not accesible
+                /*Console.WriteLine(baseObj.address);
+                Console.WriteLine(baseObj.job); 
+                Console.WriteLine(baseObj.street);
+                Console.WriteLine(baseObj.description);*/
+
+            //accesible
+            Console.WriteLine(baseObj.name);
+    }
+    }
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        /*Assembly1DerivedClass ass1deriveDoBJ= new Assembly1DerivedClass();
+        ass1deriveDoBJ.TestAcesssinDerivedClass();*/
+
+        Assembly1OtherClass objOther = new Assembly1OtherClass();
+        objOther.TestAccessinOtherClass();
+
+
+    }
+}
---
+++ SecondDayInternship/StaticMethods&Variables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+class Counter
+{
+    public static int count;   //static variable
+
+    public static void getCount()  //static method
+    {
+        ++count;
+        Console.WriteLine("Counting:{0}",count);
+    }
+
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Counter c1 = new Counter();      //Since it is static we dont need to do c1.getCount()
+        Counter.getCount();
+        Counter.getCount();
+        Counter.getCount();
+        Counter.getCount();
+        Counter.getCount();
+    }
+}
