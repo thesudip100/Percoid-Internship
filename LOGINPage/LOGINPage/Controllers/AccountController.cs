1f94720658277f570025a98b8597e86d7cfd1676
Sudip Paudel <sudippaudel944@gmail.com>
2024-06-07 15:31:53 +0545

    fifth day: login page functionality practise

---
+++ LOGINPage/LOGINPage/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using LOGINPage.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LOGINPage.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            // Validate login credentials against the database
+            var user =  _context.Users.SingleOrDefault(u => u.Username == username && u.Password == password);
+
+            if (user != null)
+            {
+                // Successful login, redirect to a dashboard or home page
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // Failed login, return to the login page with an error message
+                ViewBag.Error = "Invalid username or password";
+                return View("Index");
+            }
+        }
+    }
+}
+
---
+++ LOGINPage/LOGINPage/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LOGINPage.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LOGINPage.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<User>Users { get; set; }
+    }
+}
